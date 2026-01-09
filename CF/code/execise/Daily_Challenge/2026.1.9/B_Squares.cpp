96a47e6583053b83f319699206f88ac2ecc23db0
Sakura_luo <2234695291@qq.com>
2026-01-09 16:34:28 +0800

    daliy

---
+++ CF/code/execise/Daily_Challenge/2026.1.9/B_Squares.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define i64 long long
+#define d64 long double
+#define endl '\n'
+const i64 mod = 1e9 + 7;
+const i64 N = 3e5 + 7;
+
+void solve()
+{
+    int n, k;
+    cin >> n >> k;
+
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+
+    set<int> st(a.begin(), a.end());
+    vector<int> b(st.begin(), st.end());
+
+    if (b.size() < k)
+    {
+        cout << -1 << endl;
+        return;
+    }
+
+    cout << b[b.size() - k] << ' ' << b[b.size() - k] << endl;
+    return;
+}
+
+signed main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(0);
+    int T = 1;
+    // cin >> T;
+    while (T--)
+    {
+        solve();
+    }
+    return 0;
+}
